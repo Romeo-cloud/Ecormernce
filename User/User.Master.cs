4b468309b6ef8feca7f545393ebc255bebed0fa9
Romeo Yeboah <qwesivybez1@gmail.com>
2025-05-14 16:21:52 +0000

    Add files via upload

---
+++ User/User.Master.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;  
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ecormerce.User
+{
+	public partial class User : System.Web.UI.MasterPage
+	{
+		protected void Page_Load(object sender, EventArgs e)
+
+		{
+            // to codes is to create injection to show the carousel only at the homepage that is default.aspx
+            if (Request.Url.AbsoluteUri.ToString().Contains("Default.aspx"))
+            {
+                // load control
+                Control sliderUserControl = (Control)Page.LoadControl("SliderUserControl.ascx");
+                pnlSliderUC.Controls.Add(sliderUserControl);
+            }
+        }
+	}
+}
